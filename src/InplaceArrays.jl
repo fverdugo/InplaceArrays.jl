3ca7023a5b5d15472e17d725f0015fb3c54be762
fverdugo <fverdugo@cimne.upc.edu>
2019-10-04 15:45:18 +0200

    Added interface for InplaceArray and a conversion from AbstractArray

--- src/InplaceArrays.jl
+++ src/InplaceArrays.jl
@@ -1,5 +1,84 @@
 module InplaceArrays
 
-greet() = print("Hello World!")
+using Test
+using Base: @propagate_inbounds
+
+export InplaceArray
+export new_cache
+export getindex!
+export test_inplace_array
+
+abstract type InplaceArray{T,N} <: AbstractArray{T,N} end
+
+"""
+new_cache(a::InplaceArray)::Any
+"""
+function new_cache end
+
+"""
+getindex!(cache,a::InplaceArray,i::Integer)
+getindex!(cache,a::InplaceArray,I::Integer...)
+"""
+function getindex! end
+
+function getindex!(cache,a::InplaceArray,I::CartesianIndex)
+  getindex!(cache,a,Tuple(I)...) 
+end
+
+function test_inplace_array(
+  a::InplaceArray{T,N}, b::AbstractArray{T,N},cmp=(==)) where {T,N}
+
+  @test cmp(a,b)
+
+  cache = new_cache(a)
+
+  t = true
+  for i in eachindex(a)
+    bi = b[i]
+    ai = getindex!(cache,a,i)
+    t = t && cmp(bi,ai)
+  end
+
+  @test t
+
+  @test IndexStyle(a) == IndexStyle(b)
+
+  true
+end
+
+struct InplaceArrayFromArray{T,N,A<:AbstractArray} <: InplaceArray{T,N}
+  array::A
+
+  function InplaceArrayFromArray(array::AbstractArray{T,N}) where {T,N}
+    A = typeof(array)
+    new{T,N,A}(array)
+  end
+
+end
+
+InplaceArray(a::AbstractArray) = InplaceArrayFromArray(a)
+
+new_cache(::InplaceArrayFromArray) = nothing
+
+function getindex!(::Nothing,a::InplaceArrayFromArray,I::Integer...)
+  a.array[I...]
+end
+
+Base.size(a::InplaceArrayFromArray) = size(a.array)
+
+@propagate_inbounds function Base.getindex(
+  a::InplaceArrayFromArray,I::Integer...)
+  a.array[I...]
+end
+
+@propagate_inbounds function Base.setindex!(
+  a::InplaceArrayFromArray,v,I::Integer...)
+  a.array[I...] = v
+end
+
+function Base.IndexStyle(
+  ::Type{InplaceArrayFromArray{T,N,A}}) where {T,N,A}
+  IndexStyle(A)
+end
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,23 @@
-using InplaceArrays
-using Test
+module Runtests
 
-@testset "InplaceArrays.jl" begin
-    # Write your own tests here.
-end
+using Test
+using InplaceArrays
+
+a = collect(1:10)
+b = InplaceArray(a)
+test_inplace_array(b,a)
+
+b[3] = -1
+@test a[3] == -1
+test_inplace_array(b,a)
+
+a = rand(3,2,4)
+b = InplaceArray(a)
+test_inplace_array(b,a)
+
+a = CartesianIndices(a)
+b = InplaceArray(a)
+test_inplace_array(b,a)
+
+end # module
+
