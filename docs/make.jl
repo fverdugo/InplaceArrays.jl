eb84ef4eb08054562870f34a7c598e49628de564
Francesc Verdugo <fverdugo@cimne.upc.edu>
2019-10-11 15:59:45 +0200

    Deploying docs

--- docs/make.jl
+++ docs/make.jl
@@ -15,9 +15,7 @@ makedocs(
     modules = [InplaceArrays]
 )
 
-# Documenter can also automatically deploy documentation to gh-pages.
-# See "Hosting Documentation" and deploydocs() in the Documenter manual
-# for more information.
-#=deploydocs(
-    repo = "<repository url>"
-)=#
+deploydocs(
+    repo = "github.com/fverdugo/InplaceArrays.jl.git",
+)
+
