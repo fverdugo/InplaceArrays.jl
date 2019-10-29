99ca6353d49ef69d6040263eb745856d03b59af5
fverdugo <fverdugo@cimne.upc.edu>
2019-10-29 08:24:27 +0100

    Fixed type instability in AppliedArray

--- src/Arrays/Apply.jl
+++ src/Arrays/Apply.jl
@@ -98,13 +98,18 @@ end
 
 function array_cache(hash::Dict,a::AppliedArray)
     id = objectid(a)
+    _cache = _array_cache(hash,a)
     if haskey(hash,id)
-      cache = hash[id]
+      cache = _get_stored_cache(_cache,hash,id)
     else
-      cache = _array_cache(hash,a)
+      cache = _cache
       hash[id] = cache
     end
     cache
+end
+
+@noinline function _get_stored_cache(cache::T,hash,id)::T where T
+  hash[id]
 end
 
 function _array_cache(hash,a::AppliedArray)
