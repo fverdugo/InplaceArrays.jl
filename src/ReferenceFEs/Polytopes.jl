5315ae33fe2131660131da56d7acae08ce952ecd
Francesc Verdugo <fverdugo@cimne.upc.edu>
2019-11-14 17:47:19 +0100

    More work in docs

--- src/ReferenceFEs/Polytopes.jl
+++ src/ReferenceFEs/Polytopes.jl
@@ -1,39 +1,83 @@
 
 """
+    abstract type Polytope{D}
 
-`D` is the environment dimension (typically, 0, 1, 2, or 3d)
+Abstract type representing a polytope (i.e., a polyhedron in arbitrary dimensions).
+`D` is the environment dimension (typically, 0, 1, 2, or 3).
 This type parameter is needed since there are functions in the 
-`Polytope` interface that return containers with `Point{D}` objects
+`Polytope` interface that return containers with `Point{D}` objects.
+We adopt the [usual nomenclature](https://en.wikipedia.org/wiki/Polytope) for polytope-related objects.
+All objects in a polytope (from vertices to the polytope itself) are called *n-faces* or simply *faces*.
+The notation *n-faces* is used only when it is needed to refer to the object dimension n. Otherwise we simply
+use *face*. In addition, we say
+
+- vertex (pl. vertices): for 0-faces
+- edge: for 1-faces
+- facet: for (`D-1`)-faces
+
+The `Polytope` interface is defined by overloading the following functions
+
+- [`polytope_faces(p)`](@ref)
+- [`polytope_dimrange(p)`](@ref)
+- [`Polytope{N}(p,faceid) where N`](@ref)
+- [`vertex_coordinates(p)`](@ref)
+- [`(==)(a::Polytope{D},b::Polytope{D}) where D`](@ref)
+
+And optionally these ones:
+
+- [`edge_tangents(p)`](@ref)
+- [`facet_normals(p)`](@ref)
+- [`facet_orientations(p)`](@ref)
+- [`vertex_permutations(p)`](@ref)
+- [`polytope_vtkid(p)`](@ref)
+- [`polytope_vtknodes(p)`](@ref)
+
+The interface can be tested with the function
+
+- [`test_polytope`](@ref)
+
 """
 abstract type Polytope{D} end
 
 # Mandatory
 
 """
+    polytope_faces(p) -> Vector{Vector{Int}}
 """
 function polytope_faces(p)
   @abstractmethod
 end
 
 """
+    polytope_dimrange(p) -> Vector{UnitRange{Int}}
 """
 function polytope_dimrange(p)
   @abstractmethod
 end
 
 """
+    Polytope{N}(p,faceid) where N
+
+Returns a `Polytope{N}` object representing the `N`-face with id `faceid`.
+The value `faceid` refers to the numeration restricted to the dimension `N`
+(it starts with 1 for the first `N`-face).
 """
 function Polytope{D}(p,Dfaceid) where D
   @abstractmethod
 end
 
 """
+    vertex_coordinates(p) -> Vector{Point{D,Float64}}
 """
 function vertex_coordinates(p)
   @abstractmethod
 end
 
 """
+    (==)(a::Polytope{D},b::Polytope{D}) where D
+
+Note that the operator `==` returns `false` by default for polytopes
+of different dimensions.
 """
 function (==)(a::Polytope{D},b::Polytope{D}) where D
   @abstractmethod
@@ -46,36 +90,42 @@ end
 # Optional
 
 """
+    edge_tangents(p) -> Vector{VectorValue{D,Float64}}
 """
 function edge_tangents(p)
   @abstractmethod
 end
 
 """
+    facet_normals(p) -> Vector{VectorValue{D,Float64}}
 """
 function facet_normals(p)
   @abstractmethod
 end
 
 """
+    facet_orientations(p) -> Vector{Int}
 """
 function facet_orientations(p)
   @abstractmethod
 end
 
 """
+    vertex_permutations(p) -> Vector{Vector{Int}}
 """
 function vertex_permutations(p)
   @abstractmethod
 end
 
 """
+    polytope_vtkid(p)
 """
 function polytope_vtkid(p)
   @abstractmethod
 end
 
 """
+    polytope_vtknodes(p)
 """
 function polytope_vtknodes(p)
   @abstractmethod
@@ -84,23 +134,30 @@ end
 # Some generic API
 
 """
+    num_dims(::Type{<:Polytope{D}}) where D
+    num_dims(p::Polytope{D}) where D
+
+Returns `D`. 
 """
 num_dims(::Type{<:Polytope{D}}) where D = D
 num_dims(p::T) where T<:Polytope = num_dims(T)
 
 """
+    num_faces(p::Polytope)
 """
 function num_faces(p::Polytope)
   length(polytope_faces(p))
 end
 
 """
+    num_faces(p::Polytope,dim::Integer)
 """
 function num_faces(p::Polytope,dim::Integer)
   length(polytope_dimrange(p)[dim+1])
 end
 
 """
+    num_facets(p::Polytope)
 """
 function num_facets(p::Polytope)
   D = num_dims(p)
@@ -112,6 +169,7 @@ function num_facets(p::Polytope)
 end
 
 """
+    num_edges(p::Polytope)
 """
 function num_edges(p::Polytope)
   D = num_dims(p)
@@ -123,12 +181,14 @@ function num_edges(p::Polytope)
 end
 
 """
+    num_vertices(p::Polytope)
 """
 function num_vertices(p::Polytope)
   num_faces(p,0)
 end
 
 """
+    polytope_facedims(p) -> Vector{Int}
 """
 function polytope_facedims(p)
   n = num_faces(p)
@@ -142,6 +202,7 @@ function polytope_facedims(p)
 end
 
 """
+    polytope_offsets(p) -> Vector{Int}
 """
 function polytope_offsets(p)
   D = num_dims(p)
@@ -158,12 +219,14 @@ function polytope_offsets(p)
 end
 
 """
+    polytope_offset(p,d)
 """
 function polytope_offset(p,d)
   polytope_offsets(p)[d+1]
 end
 
 """
+    polytope_faces(p,dimfrom,dimto) -> Vector{Vector{Int}}
 """
 function polytope_faces(p,dimfrom,dimto)
   if dimfrom >= dimto
@@ -204,8 +267,12 @@ end
 # Testers
 
 """
+    test_polytope(p::Polytope{D}; optional::Bool=false) where D
 
-It does not test `polytope_vtkid`  nor `polytope_vtknodes`
+Function that stresses out the functions in the `Polytope` interface.
+with `optional=false` (the default), only the mandatory functions are tested.
+With `optional=true`, the optional functions are also tested except
+`polytope_vtkid`  nor `polytope_vtknodes`
 """
 function test_polytope(p::Polytope{D};optional::Bool=false) where D
   @test D == num_dims(p)
